ec66e5db4a56e133fdcd713c37620374d7eb9b0b
Bill Kremer <bkrems12@gmail.com>
2017-07-07 16:42:59 -0500

    cleaned up spacing for readability

--- four-action/script.js
+++ four-action/script.js
@@ -1,7 +1,4 @@
 $(function () {
-
-  var formData = {};
-
   $('form').on('submit', function (event) {
     event.preventDefault();
     var formData = {};
@@ -9,33 +6,26 @@ $(function () {
     $('#form_results').empty();
 
     var formObjArray = $(this).serializeArray();
-    console.log(formObjArray);
+    // console.log(formObjArray);
     formObjArray.forEach(function (input) {
-
       formData[input.name] = input.value;
     });
-    console.log(formData);
+    // console.log(formData);
 
     var $dataToDisplay = $("<ul><li>Name: " + formData.first_name + " " + formData.last_name + "</li>")
     $dataToDisplay.append("<li>Email: " + formData.email_address + "</li>")
     $dataToDisplay.append("<li>Company: " + formData.company_name + "</li>")
     $dataToDisplay.append("<li>Title: " + formData.title + "</li>")
     $dataToDisplay.append("<li>Country: " + formData.country + "</li>")
-console.log(formData.state);
+    // console.log(formData.state);
     if (formData.state != undefined) {
       $dataToDisplay.append("<li>State: " + formData.state + "</li>")
     }
     $dataToDisplay.append("<li>Additional Comments: " + formData.additional_comments + "</li>")
-    //   var $formData = $('<div class="'+ emp.employeeSecretID  +'"></div>');
-    //
-    //   $empFName.append('<p>' + emp.employeeFirstName + '</p>');
-    // // add data to delete button for catching the specific class for use during deletion.
-    //
-    $('#form_results').append($dataToDisplay);
-    //
-    //
 
+    $('#form_results').append($dataToDisplay);
   });
+
   // data validation to prevent states from being picked without US picked first
   $('form').find('#country').on("change", function () {
     if ($('form').find('#country').val() == "United States") {
@@ -44,4 +34,5 @@ console.log(formData.state);
       $('form').find('#state').attr('disabled',true).val('');
     }
   });
+
 });
